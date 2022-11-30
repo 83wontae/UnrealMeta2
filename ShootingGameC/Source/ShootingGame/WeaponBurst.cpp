e33ef442720a8eae082e6f92e5aecca45afdf612
junios <junios@naver.com>
2022-11-30 11:01:30 +0900

    aaa

--- ShootingGameC/Source/ShootingGame/WeaponBurst.cpp
+++ ShootingGameC/Source/ShootingGame/WeaponBurst.cpp
@@ -25,24 +25,5 @@ void AWeaponBurst::PressTrigger_Implementation(bool isPressed)
 
 void AWeaponBurst::BurstShooting()
 {
-	bool isUse = false;
-	IsCanUse(isUse);
-
-	if (isUse == false)
-		return;
-
-	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), WeaponData->FireEffect, Mesh->GetSocketLocation("Muzzle"), Mesh->GetSocketRotation("Muzzle"), FVector(0.3f, 0.3f, 0.3f));
-
-	Audio->Play();
-
-	APlayerController* shooter = GetWorld()->GetFirstPlayerController();
-	if (shooter == OwnChar->GetController())
-	{
-		FVector forward = shooter->PlayerCameraManager->GetActorForwardVector();
-
-		FVector start = (forward * 350) + shooter->PlayerCameraManager->GetCameraLocation();
-		FVector end = (forward * 5000) + shooter->PlayerCameraManager->GetCameraLocation();
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Client - ReqShoot")));
-		ReqShoot(start, end);
-	}
+	OwnChar->PlayAnimMontage(WeaponData->ShootMontage);
 }
